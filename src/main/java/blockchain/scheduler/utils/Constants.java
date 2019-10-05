35ec06dbfd49b4af6ff4e354e8c307c736b31e56
Andrzej Wilczyński <andrzejwilczynski@MacBook-Pro-Andrzej.local>
2019-10-05 14:49:46 +0200

    generate simulation data

--- src/main/java/blockchain/scheduler/utils/Constants.java
+++ src/main/java/blockchain/scheduler/utils/Constants.java
@@ -2,10 +2,10 @@ package blockchain.scheduler.utils;
 
 public class Constants
 {
-    public static final int NO_OF_TASKS = 10; // number of Cloudlets;
+    public static final int NO_OF_TASKS = 4092; // number of Cloudlets;
     public static final int NO_OF_DATA_CENTERS = 1; // number of Datacenters;
     public static final int POPULATION_SIZE = 30; // Number of Particles.
-    public static final int NO_OF_VMS = 3;
+    public static final int NO_OF_VMS = 128;
     public static final int NO_OF_ATTEMPTS = 1;
     public static final double FAILURE_COEFFICIENT = 3;
     public static final double SECURITY_LEVEL = 0.2;
